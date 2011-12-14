9c7dcf03f984e010be73c5ae6fc2e93b2ed76c70
Jasmin Beganović <bjasko@bring.out.ba>
2011-12-14 19:04:28 +0100

    ver 0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+0.1.0  2011-12-14, init
+
+  - [#25555](http://redmine.bring.out.ba/issues/25555)
--- F18_3rd_party_ubuntu_install.sh
+++ F18_3rd_party_ubuntu_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VER=0.2.0
+VER=0.1.0
 DAT=14.12.2011
 F18INSTALL=~/bin
 ARCH=`uname -m`
@@ -19,7 +19,7 @@ sudo apt-get install libqt4-sql-psql
 sudo apt-get install wine
 sudo apt-get install vim-gtk
 sudo apt-get install wget 
-wget http://winetricks.org/winetricks 
+wget -N http://winetricks.org/winetricks 
 chmod +x winetricks
 sh winetricks -q  riched20
 
@@ -36,26 +36,34 @@ fi
 
 echo " instaliram F18"
 
+mkdir tmp 
+cd tmp 
+
 wget -N http://knowhow-erp-f18.googlecode.com/files/F18_Ubuntu_"$ARCH"_"$F18_VER".gz
-wget -N http://knowhow-erp-f18.googlecode.com/files/delphirb_%DELRB_VER%.gz
-wget -N http://knowhow-erp-f18.googlecode.com/files/ptxt_%PTXT_VER%.gz
+wget -N http://knowhow-erp-f18.googlecode.com/files/delphirb_$DELRB_VER.gz
+wget -N http://knowhow-erp-f18.googlecode.com/files/ptxt_$PTXT_VER.gz
 
+gzip -dN ptxt_$PTXT_VER.gz
+gzip -dN delphirb_$DELRB_VER.gz
+gzip -dN F18_Ubuntu_"$ARCH"_"$F18_VER".gz
 
-cp bin/F18 $F18INSTALL
+cp F18 $F18INSTALL
 chmod +x $F18INSTALL/F18
+cd ..
 
-echo "deps" 
+echo "kopiram utils" 
 
-cp util/PTXT  $F18INSTALL
-cp util/ptxt.exe ~/.wine/drive_c/
-cp util/delphirb.exe ~/.wine/drive_c/
+cp util/ptxt  $F18INSTALL
+cp tmp/ptxt.exe ~/.wine/drive_c/
+cp tmp/delphirb.exe ~/.wine/drive_c/
 cp fonts/ptxt_fonts/*.ttf  ~/.wine/drive_c/windows/Fonts/
 cp util/F18_ubuntu_update  $F18INSTALL
 cp util/f18_editor $F18INSTALL
-chmod +x $F18INSTALL/update
+chmod +x $F18INSTALL/F18_ubuntu_update
 chmod +x $F18INSTALL/PTXT
 chmod +x $F18INSTALL/f18_editor
 
+echo ".....OK zavrsio sa kopiranjem"
 
 echo "setujem envars"
 
