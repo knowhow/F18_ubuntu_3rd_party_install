d1b703c0c5d6382622e48709d36212033fce9238
Jasmin Beganović <bjasko@bring.out.ba>
2011-12-14 17:28:33 +0100

    init

---
+++ F18_3rd_party_ubuntu_install.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+VER=0.2.0
+DAT=14.12.2011
+F18INSTALL=~/bin
+ARCH=`uname -m`
+DELRB_VER="1.0"
+PTXT_VER="1.55"
+F18_VER="0.9.18"
+echo "F18 install app ver: $VER, dat: $DAT"
+echo "F18 lin installacija ...."
+
+
+
+echo "F18 req."
+
+sudo apt-get update
+sudo apt-get install libqt4-sql-psql
+sudo apt-get install wine
+sudo apt-get install vim-gtk
+sudo apt-get install wget 
+wget http://winetricks.org/winetricks 
+chmod +x winetricks
+sh winetricks -q  riched20
+
+
+echo " postoji li F18 install dir"
+
+if [ -d $F18INSTALL ]; then
+
+	echo "F18 instalaciona lokacija postoji, nastavljam" 
+else
+        echo "kreiram F18 install dir"
+ 	mkdir -p  $F18INSTALL
+fi
+
+echo " instaliram F18"
+
+wget -N http://knowhow-erp-f18.googlecode.com/files/F18_Ubuntu_"$ARCH"_"$F18_VER".gz
+wget -N http://knowhow-erp-f18.googlecode.com/files/delphirb_%DELRB_VER%.gz
+wget -N http://knowhow-erp-f18.googlecode.com/files/ptxt_%PTXT_VER%.gz
+
+
+cp bin/F18 $F18INSTALL
+chmod +x $F18INSTALL/F18
+
+echo "deps" 
+
+cp util/PTXT  $F18INSTALL
+cp util/ptxt.exe ~/.wine/drive_c/
+cp util/delphirb.exe ~/.wine/drive_c/
+cp fonts/ptxt_fonts/*.ttf  ~/.wine/drive_c/windows/Fonts/
+cp util/F18_ubuntu_update  $F18INSTALL
+cp util/f18_editor $F18INSTALL
+chmod +x $F18INSTALL/update
+chmod +x $F18INSTALL/PTXT
+chmod +x $F18INSTALL/f18_editor
+
+
+echo "setujem envars"
+
+echo export PATH=\$PATH:~/bin >> ~/.bash_profile
+echo ""
+echo ""
+echo "F18 instalacija zavrsena, pokrecemo iz terminala sa F18 "
+
+
+
